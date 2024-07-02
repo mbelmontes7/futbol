9efd312830c0e5876b935012ebdc1cb81e504bb6
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-07-02 13:05:00 -0500

    chore: Update npm dependencies and tailwind configuration, also added landing page text also the typewriter on the title it looks very nice!!

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-// import TypewriterTitle from "@/components/ui/TypewriterTitle";
+"use client";
+import TypewriterTitle from "@/components/TypewriterTitle";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
@@ -8,16 +9,16 @@ export default function Home() {
     //this is my stle for the background of the landig page
     <div className="bg-gradient-to-r min-h-screen grainy from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h1 className="font-semibold text-7xl text-center">
-          Futbolnet <span className="text-green-600 font-bold">to make friends</span>{" "}
-          and learn from each other
+        <h1 className="font-semibold text-3xl text-center">
+          <TypewriterTitle /> <span className="text-green-600 font-bold">to make friends </span>{" "}
+
         </h1>
         <div className="mt-4"></div>
         <h2 className="font-semibold text-3xl text-center text-slate-700">
-          {/* <TypewriterTitle /> */}
+
+
         </h2>
         <div className="mt-8"></div>
-
         <div className="flex justify-center">
           {/* <Link href="/dashboard"> */}
           {/* <Button className="bg-green-600">
---
+++ components/TypewriterTitle.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Typewriter from 'typewriter-effect'
+
+
+type Props = {}
+
+const TypewriterTitle = (props: Props) => {
+    return (
+        <Typewriter
+            options={{
+                loop: true,
+
+            }}
+            onInit={(typewriter) => {
+                typewriter
+                    .typeString("An app for futbol lovers")
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .typeString("Futbolnet")
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .typeString("Soccer")
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .typeString("The Beautiful Game  ")
+                    .pauseFor(1000)
+                    .deleteAll()
+                    .typeString("Futbolnet")
+                    .start();
+            }}
+
+
+        />
+
+    );
+};
+
+export default TypewriterTitle
