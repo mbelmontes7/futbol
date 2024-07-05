232dce7f518ac4bc50cd082abd1d5ec57b4a31ee
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-07-04 22:06:29 -0500

    chore: Update npm dependencies, add daisyui plugin to tailwind configuration
    - Updated the layout to ensure the "Sign Up" button is centered below the "make friends" text.
    - Wrapped the button in a div with margin-top to add space between the text and the button.
    - Ensured the hero section uses flexbox to center content both horizontally and vertically.

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,30 @@
+"use client";
+import { Button } from '@/components/ui/button'
+import { SignedIn, SignedOut } from '@clerk/nextjs'
 import React from 'react'
 
 type Props = {}
 
 const Onboarding = (props: Props) => {
     return (
-        <div>Onboarding</div>
+        <div>
+            <SignedIn>
+                <div>Onboarding</div>
+            </SignedIn>
+            <SignedOut>
+                <div className="flex flex-col items-center justify-center">
+                    Sign in to continue
+
+                    <Button
+                        className="mt-4"
+                        onClick={() => {
+                            window.location.href = "/";
+                        }}
+                    >Sign In
+                    </Button>
+                </div>
+            </SignedOut>
+        </div>
     )
 }
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,58 +1,54 @@
 "use client";
 import TypewriterTitle from "@/components/TypewriterTitle";
 import { Button } from "@/components/ui/button";
-// import { Button } from "@/components/ui/button";
-// import Link from "next/link";
-// import { ArrowRight } from "lucide-react";
 import {
   ClerkProvider,
   SignInButton,
   SignedIn,
   SignedOut,
   UserButton
-} from '@clerk/nextjs'
-
-
+} from '@clerk/nextjs';
 
 export default function Home() {
   return (
-    <div>
-      <div className="bg-teal-100 from-green-100">
-        <ClerkProvider>
-          <SignedOut>
-            <button
-              className="rounded-xl text-1xl font-bold p-3 bg-green-200 text-green-800 border border-green-500 hover:bg-green-300 hover:text-green-900 focus:outline-none focus:ring-2 focus:ring-green-400 transition duration-300 ease-in-out font-sans"
+    <ClerkProvider>
+      <SignedIn>
+        {/* User is signed in */}
+        <div className="flex flex-col justify-center items-center bg-teal-100 from-green-100">
+          <UserButton />
+        </div>
 
-            >
+        <div className="flex flex-col items-center justify-center">
+          Thanks for signing in.
+          <Button
+            className="mt-4"
+            onClick={() => {
+              window.location.href = "/onboarding";
+            }}
+          >
+            Dashboard
+          </Button>
+        </div>
+      </SignedIn>
+
+      <SignedOut>
+        {/* This is my hero section */}
+        <div className=" bg-gradient-to-r min-h-screen grainy from-green-200 to-teal-100 flex flex-col justify-center items-center relative">
+          {/* Centered TypewriterTitle */}
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+            <h1 className="font-semibold text-3xl">
+              <TypewriterTitle />{" "}
+              <span className="text-green-600 font-bold text-2xl">to make friends </span>
+            </h1>
+          </div>
+          {/* Positioned Button on the button of the page but keeping it on the center.. So I added a div with a margin of 36 */}
+          <div className=" mt-40">
+            <Button className="rounded-xl text-1xl font-bold p-3 bg-green-200 text-green-800 border border-green-500 hover:bg-green-300 hover:text-green-900 focus:outline-none focus:ring-2 focus:ring-green-400 transition duration-300 ease-in-out font-sans">
               <SignInButton />
-            </button>
-
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </ClerkProvider>
-      </div>
-
-      <div className="bg-gradient-to-r min-h-screen grainy from-green-200 to-teal-100">
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          <h1 className="font-semibold text-3xl text-center">
-            <TypewriterTitle /> <span className="text-green-600 font-bold">to make friends </span>{" "}
-          </h1>
-          <div className="mt-4"></div>
-          <h2 className="font-semibold text-3xl text-center text-slate-700"></h2>
-          <div className="mt-8"></div>
-          <div className="flex justify-center">
-            {/* <Link href="/Onbording"> */}
-            {/* // <Button className="bg-green-600">
-            //   Get Started
-            //   <ArrowRight className="ml-2 w-5 h-5" strokeWidth={3} />
-            // </Button>
-            </Link> */}
+            </Button>
           </div>
         </div>
-      </div>
-    </div>
-
+      </SignedOut>
+    </ClerkProvider>
   );
 }
