136aaaffc66f50a5a95b270cfaa641f120247847
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-07-10 15:49:53 -0500

    chore: Update Bottombar component for mobile view
    - Implement responsive design for Bottombar component to display only on mobile view when the user is on the home page or viewing other people's posts.
    - Add necessary imports and dependencies.
    - Include sidebar links and apply styling to the component.

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,8 +1,47 @@
+//this bottombar is going to be for moblie view only when the user is on the home page or is on  looking at the things the other people have posted
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { sidebarLinks } from "@/constants";
+
 function Bottombar() {
+    const pathname = usePathname();
+
     return (
-        <div className="text-black absolute bottom-2">
-            <p>Bottombar</p>
-        </div>
+        <section className='bottombar'>
+            <div className='bottombar_container'>
+                {sidebarLinks.map((link) => {
+                    const isActive =
+                        (pathname.includes(link.route) && link.route.length > 1) ||
+                        pathname === link.route;
+
+                    return (
+                        <Link
+                            href={link.route}
+                            key={link.label}
+                            //special bar for the bottom bar
+                            className={`bottombar_link ${isActive && "bg-emerald-400 "}`}
+                        >
+                            <Image
+                                src={link.imgURL}
+                                alt={link.label}
+                                width={16}
+                                height={16}
+                                className='object-contain'
+                            />
+
+                            <p className='text-subtle-medium text-light-1 max-sm:hidden'>
+                                {link.label.split(/\s+/)[0]}
+                            </p>
+                        </Link>
+                    );
+                })}
+            </div>
+        </section>
     );
 }
+
 export default Bottombar;
