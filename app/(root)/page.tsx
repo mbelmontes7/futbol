2955f3144bd30191c76e1dbcd41f9dbd595f84b8
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-07-24 16:40:45 -0500

    The app is now connected to the database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <div>
+        {/* //this is the main container that is going to be displayed on the screen  the flex and flex-row is for the Right side bar */}
       <h1 className="head-text text-green-400 ">Home</h1>
     </div>
   );
