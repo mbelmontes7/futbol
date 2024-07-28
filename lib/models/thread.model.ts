f7ce9c0a5374ec543827edc9a42c07b7e29d4932
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-07-27 23:40:15 -0500

    Added a thread schema with recursive children to allow threads to have nested replies. This is on the thread model file

--- lib/models/thread.model.ts
+++ lib/models/thread.model.ts
@@ -1,32 +1,33 @@
 import mongoose from "mongoose";
-
+//one thread can have multiple children this is recursion in action
+//This creates a structure where threads can keep nesting inside each other, like a conversation with replies to replies to replies.
 const threadSchema = new mongoose.Schema({
-  text: {
-    type: String,
-    required: true,
-  },
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  community: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Community",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  parentId: {
-    type: String,
-  },
-  children: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Thread",
+    text: {
+        type: String,
+        required: true, // Every thread must have text
     },
-  ],
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", // This refers to a User who created the thread
+        required: true,
+    },
+    community: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Community", // This refers to a Community where the thread is posted
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now, // This sets the date and time when the thread was created
+    },
+    parentId: {
+        type: String, // This is the ID of the parent thread, if this is a reply
+    },
+    children: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Thread", // This refers to child threads or replies to this thread
+        },
+    ],
 });
 
 const Thread = mongoose.models.Thread || mongoose.model("Thread", threadSchema);
