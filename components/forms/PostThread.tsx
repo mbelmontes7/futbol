393dec0145a66c410023cd142f084be67655e463
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-07-29 23:28:39 -0500

    So,
    -the user auth is not showign anymore
    -the create post is also not working ..

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -20,6 +20,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
 
+
 interface Props {
   userId: string;
 }
@@ -58,7 +59,7 @@ function PostThread({ userId }: Props) {
     <Form {...form}>
       <form
         className='mt-10 flex flex-col justify-start gap-10'
-        onSubmit={form.handleSubmit(onSubmit)}
+      // onSubmit={form.handleSubmit(onSubmit)}
       >
         <FormField
           control={form.control}
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,4 +1,3 @@
-"use server";
 import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
@@ -11,36 +10,16 @@ interface Params {
     path: string,
 }
 //users can create their own post and threads
-export async function createThread({ text, author, communityId, path }: Params
-) {
-    try {
-        connectToDB();
-
-        // const communityIdObject = await Community.findOne(
-        //   { id: communityId },
-        //   { _id: 1 }
-        // );
-
-        const createdThread = await Thread.create({
-            text,
-            author,
-            //   community: communityIdObject, // Assign communityId if provided, or leave it null for personal account
-        });
-
-        // Update User model
-        await User.findByIdAndUpdate(author, {
-            $push: { threads: createdThread._id },
-        });
-
-        // if (communityIdObject) {
-        //   // Update Community model
-        //   await Community.findByIdAndUpdate(communityIdObject, {
-        //     $push: { threads: createdThread._id },
-        //   });
-        // }
-
-        revalidatePath(path);
-    } catch (error: any) {
-        throw new Error(`Failed to create thread: ${error.message}`);
-    }
+export async function createThread({ text, author, communityId, path }: Params) {
+    connectToDB();
+    const createdThread = await Thread.create({
+        text,
+        author,
+        community: null,
+    });
+    //update the user model with the new thread
+    await User.findByIdAndUpdate(author, {
+        $push: { threads: createdThread._id },
+    })
+    revalidatePath(path);
 }
