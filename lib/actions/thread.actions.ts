4c4e427bbfc5af94e6182908650dbabf21aa7753
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-08-01 13:18:30 -0500

    feat: post actions for the user
    this is on the thread actions for the create post:  Calculate the number of posts to skip based on the page number and page size. Modify the postQuery to sort the posts by createdAt in descending order, skip the calculated amount of posts, and limit the number of posts to the page size. Populate the author field with the User model and populate the children field with the User model, selecting only the _id, name, parentId, and image fields. This improves the efficiency of fetching posts and provides necessary data for rendering the posts and their authors.

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -25,9 +25,24 @@ export async function createThread({ text, author, communityId, path }: Params) 
     revalidatePath(path);
 }
 //fetch the posts 
-export async function fetchPosts(pageNumber =1, pageSize = 20) {
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
     //establish a connection to the database
     connectToDB();
+    //calculate the number of posts to skip
+    const skipAmount = pageSize * (pageNumber - 1);
+
     //Fetch the posts from the database adn that have no parents 
-    const postQuery = Thread.find()
+    const postQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+        .sort({ createdAt: 'desc' })
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: 'author', model: 'User' })
+        .populate({
+            path: 'children',
+            populate: {
+                path: 'author', model: 'User',
+                select: "_id name parentId image"
+            }
+
+        })
 }
