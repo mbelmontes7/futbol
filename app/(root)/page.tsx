9f0643d987e499882b623a0cc7367d0f3a189c91
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-08-03 13:47:17 -0700

    feat: The user now can see the post on home page
    The function now takes two optional parameters, pageNumber and pageSize, to specify the desired page of posts and the number of posts per page. This improves the efficiency of fetching posts and allows for better control over the displayed content.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,53 @@
 
+import { redirect } from "next/navigation";
+
+
+// import Pagination from "@/components/shared/Pagination";
+
 import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+import ThreadCard from "@/components/cards/ThreadCard";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default async function Home() {
-  //in here we are going to fetch the posts from the database
+
+export async function home() {
   const result = await fetchPosts(1, 30);
-
+  const user = await currentUser();
 
 
   return (
     <>
-      {/* //this is the main container that is going to be displayed on the screen  the flex and flex-row is for the Right side bar */}
-      <h1 className="head-text text-green-400 ">Home</h1>
+      <h1 className='head-text text-left'>Home</h1>
+
+      <section className='mt-9 flex flex-col gap-10'>
+        {result.posts.length === 0 ? (
+          <p className='no-result'>No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id || ""}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
+
+      {/* <Pagination
+        path='/'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      /> */}
     </>
   );
 }
+
+export default home;
