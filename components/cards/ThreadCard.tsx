dacb8ccb1e957882df9363345b5dc024756a1da5
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-08-03 19:26:52 -0700

    reafactor: created the images where the author is there bassically every time the user post you can see who posted what on the main page where is going to be on the home page

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -42,7 +42,20 @@ const ThreadCard = ({
 }: Props) => {
     return (
         <article className="flex w-full flex-col rounded-xl bg-lime-100 p-7">
-            
+            <div className="flex items-start justify-between"></div>
+            <div className="flex w-full flex-1 flex-grow gap-4"></div>
+           {/* //this is the image of the user is going to be when the user post a comment on the social media */}
+            <Link href={`/profile/${author.id}`} className='relative h-11 w-11'>
+                <Image
+                    src={author.image}
+                    alt='user_community_image'
+                    fill
+                    className='cursor-pointer rounded-full'
+                />
+            </Link>
+
+
+
             <h2 className="text-small-regular text-black">
                 {content}
             </h2>
