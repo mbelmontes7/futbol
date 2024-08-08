00d288d900594af6ebd24a2b53b9a4edbc146e9f
mbelmontes7 <85905636+mbelmontes7@users.noreply.github.com>
2024-08-07 22:15:18 -0500

    feat: Refactor ThreadCard component to include user name and image
    Refactor the ThreadCard component to include the user name and image. This enhances the user experience by providing more context and visual representation of the author when viewing posts.

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -41,72 +41,93 @@ const ThreadCard = ({
     isComment,
 }: Props) => {
     return (
-        <article className="flex w-full flex-col rounded-xl bg-zinc-900 p-7">
-            <div className="flex items-start justify-between"></div>
-            <div className="flex w-full flex-1 flex-grow gap-4"></div>
-            {/* //this is the image of the user is going to be when the user post a comment on the social media */}
-            <Link href={`/profile/${author.id}`} className='relative h-11 w-11'>
-                <Image
-                    src={author.image}
-                    alt='user_community_image'
-                    fill
-                    className='cursor-pointer rounded-full'
-                />
-            </Link>
-            {/* //created space for the card space and is render from the file of the globals.cs */}
-            <div className='thread-card_bar' />
-            <div className='flex w-full flex-col'>
-                {/* this is the user uname of the bottom of the comment  */}
-                <Link href={`/profile/${author.id}`} className='w-fit'>
-                    <h4 className='cursor-pointer text-base-medium text-green-100 m-1'>
-                        {author.name}
-                    </h4>
-                </Link>
-            </div>
-            {/* //Render the content of the post */}
-            <p className='mt-2 text-body-semibold text-green-200'>{content}</p>
-            {/* //Render the community image  here are all my icons for the app that it can see the user */}
-            <div className={`${isComment && "mb-10"} mt-5 flex flex-col gap-3`}>
-                <div className='flex gap-3.5'>
-                    <Image
-                        src='/assets/heart-gray.svg'
-                        alt='heart'
-                        width={24}
-                        height={24}
-                        className='cursor-pointer object-contain'
-                    />
-                    {/* //this is the reply icon for the user to reply to the post */}
-                    <Link href={`/thread/${id}`}>
-                        <Image
-                            src='/assets/reply.svg'
-                            alt='heart'
-                            width={24}
-                            height={24}
-                            className='cursor-pointer object-contain'
-                        />
-                    </Link>
-                    <Image
-                        src='/assets/repost.svg'
-                        alt='heart'
-                        width={24}
-                        height={24}
-                        className='cursor-pointer object-contain'
-                    />
-                    <Image
-                        src='/assets/share.svg'
-                        alt='heart'
-                        width={24}
-                        height={24}
-                        className='cursor-pointer object-contain'
-                    />
-                    {/* // part of the comment section for a user. It conditionally renders a link to the post  if there are comments to */}
-                    {isComment && comments.length > 0 && (
-                        <Link href={`/thread/${id}`}>
-                            <p className='mt-1 text-subtle-medium text-gray-1'>
-                                {comments.length} repl{comments.length > 1 ? "ies" : "y"}
-                            </p>
+        <article
+            className={`flex w-full flex-col rounded-xl ${isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
+                }`}
+        >
+            <div className='flex items-start justify-between'>
+                <div className='flex w-full flex-1 flex-row gap-4'>
+                    <div className='flex flex-col items-center'>
+                        <Link href={`/profile/${author.id}`} className='relative h-11 w-11'>
+                            <Image
+                                src={author.image}
+                                alt='user_community_image'
+                                fill
+                                className='cursor-pointer rounded-full'
+                            />
                         </Link>
-                    )}
+
+                        <div className='thread-card_bar' />
+                        {/* this div provides the line on the name*/}
+                    </div>
+
+                    <div className='flex w-full flex-col'>
+                        <Link href={`/profile/${author.id}`} className='w-fit'>
+                            <h4 className='cursor-pointer text-base-semibold text-light-1'>
+                                {/* {author.name} */}
+                            </h4>
+                        </Link>
+
+
+
+                        <div className='flex w-full flex-col'>
+                            {/* this is the user uname of the bottom of the comment  */}
+                            <Link href={`/profile/${author.id}`} className='w-fit'>
+                                {/* //to make it appear on the screen */}
+                                <h4 className='cursor-pointer text-base-medium text-green-100 m-1'>
+                                    {author.name}
+                                </h4>
+                            </Link>
+                        </div>
+                        {/* //Render the content of the post */}
+                        <p className='mt-2 text-body-semibold text-green-200'>{content}</p>
+
+                        {/* //Render the community image  here are all my icons for the app that it can see the user */}
+                        <div className={`${isComment && "mb-10"} mt-5 flex flex-col gap-3`}>
+                            {/* //social media icons for the user to interact with the post */}
+                            <div className='flex gap-3.5'>
+                                <Image
+                                    src='/assets/heart-gray.svg'
+                                    alt='heart'
+                                    width={24}
+                                    height={24}
+                                    className='cursor-pointer object-contain'
+                                />
+                                {/* //this is the reply icon for the user to reply to the post and the only one that is going to be working right now */}
+                                <Link href={`/thread/${id}`}>
+                                    <Image
+                                        src='/assets/reply.svg'
+                                        alt='heart'
+                                        width={24}
+                                        height={24}
+                                        className='cursor-pointer object-contain'
+                                    />
+                                </Link>
+                                <Image
+                                    src='/assets/repost.svg'
+                                    alt='heart'
+                                    width={24}
+                                    height={24}
+                                    className='cursor-pointer object-contain'
+                                />
+                                <Image
+                                    src='/assets/share.svg'
+                                    alt='heart'
+                                    width={24}
+                                    height={24}
+                                    className='cursor-pointer object-contain'
+                                />
+                                {/* // part of the comment section for a user. It conditionally renders a link to the post  if there are comments to */}
+                                {isComment && comments.length > 0 && (
+                                    <Link href={`/thread/${id}`}>
+                                        <p className='mt-1 text-subtle-medium text-gray-1'>
+                                            {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+                                        </p>
+                                    </Link>
+                                )}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
